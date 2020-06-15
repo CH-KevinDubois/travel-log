1442ae7dd41cb35e76d462869995770747e09fbe
CH-KevinDubois <kvn.dubois@gmail.com>
2020-06-15 21:00:32 +0200

    Fin de l'initialistion step by step

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,36 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DummyPageComponent } from './dummy-page/dummy-page.component';
+import { LoginPageComponent } from './security/login-page/login-page.component';
+import { LogoutButtonComponent } from './security/logout-button/logout-button.component';
+import { ApiTokenInterceptorService } from './api/api-token-interceptor.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginPageComponent,
+    DummyPageComponent,
+    LogoutButtonComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule,
+    AppRoutingModule,
+    HttpClientModule,
+    ReactiveFormsModule,      
   ],
-  providers: [],
+  providers: [
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: ApiTokenInterceptorService,
+      multi: true,
+    },
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/security/security.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { LoginPageComponent } from './login-page/login-page.component';
+import { LogoutButtonComponent } from './logout-button/logout-button.component';
+
+
+
+@NgModule({
+  declarations: [LoginPageComponent, LogoutButtonComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class SecurityModule { }

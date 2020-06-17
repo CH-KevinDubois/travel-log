12280e51e96ff64a4bdba225ed9a1d8d88325d08
CH-KevinDubois <kvn.dubois@gmail.com>
2020-06-17 22:54:17 +0200

    Création classe Trip

---
+++ src/app/models/trip.ts
@@ -0,0 +1,11 @@
+export class Trip {
+    id: string;
+    href: string;
+    title: string;
+    description: string;
+    placesCount: number;
+    userId: string;
+    userRef: string;
+    createdAt: Date;
+    updateAt: Date;
+}

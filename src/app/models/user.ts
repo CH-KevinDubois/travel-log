05c559129b592f4de5fab18f842203e2b9252ff9
CH-KevinDubois <kvn.dubois@gmail.com>
2020-06-18 19:30:10 +0200

    Ajout constructeur

--- src/app/models/user.ts
+++ src/app/models/user.ts
@@ -5,4 +5,13 @@ export class User {
     tripsCount: number;
     createdAt: Date;
     updatedAt: Date;
+
+    constructor(data: Partial<User> = { }) {
+        this.id = data.id;
+        this.href = data.href;
+        this.name = data.name;
+        this.tripsCount = data.tripsCount;
+        this.createdAt = data.createdAt;
+        this.updatedAt = data.updatedAt;
+    }
 }

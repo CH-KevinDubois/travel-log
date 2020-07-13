a5916181ce7314affa597209c40e0da990f81c07
CH-KevinDubois <kvn.dubois@gmail.com>
2020-07-13 22:40:28 +0200

    Ajout d'un menu déroulant

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
+
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatIconModule } from '@angular/material/icon'; 
 import { MatLineModule } from '@angular/material/core';
 import { MatButtonModule } from '@angular/material/button'; 
+import { MatMenuModule, matMenuAnimations } from '@angular/material/menu';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -33,6 +35,7 @@ import { RouterModule } from '@angular/router';
     MatToolbarModule,
     MatIconModule, 
     MatButtonModule,
+    MatMenuModule,
     RouterModule, 
   ],
   providers: [

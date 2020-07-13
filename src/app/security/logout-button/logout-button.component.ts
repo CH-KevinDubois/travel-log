76e40b4a0a80645f8f2978f1822b200c01ffd4d9
CH-KevinDubois <kvn.dubois@gmail.com>
2020-07-13 21:42:50 +0200

    Appliquer Material et styling du logout-button

--- src/app/security/logout-button/logout-button.component.ts
+++ src/app/security/logout-button/logout-button.component.ts
@@ -4,7 +4,18 @@ import { Router } from "@angular/router";
 
 @Component({
   selector: "app-logout-button",
-  template: '<button (click)="logout()">Logout</button>',
+    template: `
+    <button mat-button color="warn" class="mat-focus-indicator mat-raised-button button-menu" (click)="logout()">
+            <span class="mat-button-wrapper">
+                <span>LoGout</span>
+            </span>
+            <div matripple="" class="mat-ripple mat-button-ripple"></div>
+            <div class="mat-button-focus-overlay"></div>
+        </button>
+    `,
+    styles : ['.button-menu {font-size: 1em;padding: 2px 10px;} '],
+
+    //<button (click)="logout()">Logout</button>
 })
 export class LogoutButtonComponent {
   constructor(private auth: AuthService, private router: Router) {}

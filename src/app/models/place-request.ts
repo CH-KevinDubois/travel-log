7e9dfb9c13e5e81f7f4d6e1a5a80dbd182eab2b0
CH-KevinDubois <kvn.dubois@gmail.com>
2020-07-31 22:35:08 +0200

    Ecriture de la classe

--- src/app/models/place-request.ts
+++ src/app/models/place-request.ts
@@ -1,2 +1,19 @@
+import { GeoJsonLocation } from './geo-json-location';
+
 export class PlaceRequest {
+    name: String;
+    description: string;
+    location: GeoJsonLocation;
+    tripHref?: String;
+    tripId?: String;
+    pictureUrl?: String;
+
+    constructor(data: Partial<PlaceRequest> = { }) {
+        this.name = data.name;
+        this.description = data.description;
+        this.location = data.location;
+        this.tripHref = data.tripHref;
+        this.tripId = data.tripId;
+        this.pictureUrl = data.pictureUrl;
+    }
 }

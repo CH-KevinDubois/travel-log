d0bccc54d2b57db7f126203ef82743b6ec4b2ee0
CH-KevinDubois <kvn.dubois@gmail.com>
2020-07-31 21:56:38 +0200

    Ajout retrieveOwnTrip, fonction pour recherche ses propres voyages

--- src/app/my-trips-page/my-trips-page.component.ts
+++ src/app/my-trips-page/my-trips-page.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
+import { Trip } from '../models/trip';
+import { MatTableDataSource } from '@angular/material/table';
+import { TripService } from '../api/services/trip.service';
+import { HttpClient } from '@angular/common/http';
+import { AuthService } from '../security/auth.service';
 
 @Component({
   selector: 'app-my-trips-page',
@@ -6,10 +11,53 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./my-trips-page.component.scss']
 })
 export class MyTripsPageComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  
+  myTrips: Trip[];
+  dataSource: MatTableDataSource<Trip>;
+  selectedTrip: Trip;
+  
+  displayedColumns: string[] = ['title'];
+  
+  constructor(
+    private tripService: TripService,
+    private http: HttpClient,
+    private changeDetectorRefs: ChangeDetectorRef,
+    private auth: AuthService
+    ) {
+      this.selectedTrip = new Trip();
+    }
+    
+    ngOnInit(): void {
+      this.retrieveOwnTrips();
+    }
+    
+    retrieveOwnTrips(){
+      let myId: string;
+      this.auth.getUser().subscribe({
+        next: (user) => {
+          myId = user.id
+        },
+        error: _ => console.log('Cannot retrive user trips')
+      });
+      this.tripService.retrievePersonalTrips(myId).subscribe({
+        next: (trips) => {
+          this.dataSource = new MatTableDataSource(trips);
+          //this.changeDetectorRefs.detectChanges();
+        },
+        error: err => {
+          console.log(err.status);
+        }
+      })
+    }
+    
+    logRow(row){
+      console.log(row);
+      this.selectedTrip = this.selectedTrip.title === row.title ? new Trip(): row;
+    }
+    
+    edit(row){
+      console.log(`double ${row}`);
+    }
+    
   }
-
-}
+  

1ad640c7f989248e962a79550e753e97fa8807e8
CH-KevinDubois <kvn.dubois@gmail.com>
2020-07-31 22:34:35 +0200

    Création de l'objet type "Point"

--- src/app/models/geo-json-location.ts
+++ src/app/models/geo-json-location.ts
@@ -1,2 +1,9 @@
 export class GeoJsonLocation {
+    type: string;
+    coordinates: number[];
+
+    constructor(x: number, y: number){
+        this.type="Point";
+        this.coordinates = [x, y];
+    }
 }

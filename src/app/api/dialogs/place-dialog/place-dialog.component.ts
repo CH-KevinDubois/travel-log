da2d969dffe2a6cd1a51abae9b652335124814e2
CH-KevinDubois <kvn.dubois@gmail.com>
2020-08-02 20:04:59 +0200

    Création du formulaire

--- src/app/api/dialogs/place-dialog/place-dialog.component.ts
+++ src/app/api/dialogs/place-dialog/place-dialog.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
+import { FormControl, Validators, MaxLengthValidator } from '@angular/forms';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { Place } from 'src/app/models/place';
 
 @Component({
   selector: 'app-place-dialog',
@@ -6,10 +9,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./place-dialog.component.scss']
 })
 export class PlaceDialogComponent implements OnInit {
+  
+  nameControl: FormControl;
+  descriptionControl: FormControl;
+  pictureUrlControl: FormControl;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(
+    public dialogRef: MatDialogRef<PlaceDialogComponent>,
+    @Inject(MAT_DIALOG_DATA) public place: Place) 
+    {
+      this.nameControl = new FormControl('', [Validators.required, Validators.minLength(3), Validators.maxLength(100)]);
+      this.descriptionControl = new FormControl('', [Validators.required, Validators.minLength(5), Validators.maxLength(50000)]); 
+      this.pictureUrlControl = new FormControl('', [Validators.minLength(10), Validators.maxLength(500)]);  
+    }
+    
+    onNoClick(): void {
+      this.dialogRef.close();
+    }
+    
+    ngOnInit(): void {
+    }
   }
-
-}
+  

a1972f4866c4aa0ca90c519b2d5a4f38215dab61
CH-KevinDubois <kvn.dubois@gmail.com>
2020-08-12 21:01:51 +0200

    Ajout champs longitude et latitues avec les controles

--- src/app/api/dialogs/place-dialog/place-dialog.component.ts
+++ src/app/api/dialogs/place-dialog/place-dialog.component.ts
@@ -16,6 +16,8 @@ export class PlaceDialogComponent implements OnInit {
   nameControl: FormControl;
   descriptionControl: FormControl;
   pictureUrlControl: FormControl;
+  longitudeControl: FormControl;
+  latitudeControl: FormControl;
 
   mapClickedSubscription: Subscription;
 
@@ -26,7 +28,9 @@ export class PlaceDialogComponent implements OnInit {
     {
       this.nameControl = new FormControl('', [Validators.required, Validators.minLength(3), Validators.maxLength(100)]);
       this.descriptionControl = new FormControl('', [Validators.required, Validators.minLength(5), Validators.maxLength(50000)]); 
-      this.pictureUrlControl = new FormControl('', [Validators.minLength(10), Validators.maxLength(500)]);  
+      this.pictureUrlControl = new FormControl('', [Validators.minLength(10), Validators.maxLength(500)]);
+      this.longitudeControl = new FormControl('', [Validators.min(-180), Validators.max(180)]);
+      this.latitudeControl = new FormControl('', [Validators.min(-90), Validators.max(90)]);  
     }
     
     onNoClick(): void {

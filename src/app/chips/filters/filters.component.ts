979396ffa95db441e506187bb8a9bc7b4f24dd55
CH-KevinDubois <kvn.dubois@gmail.com>
2020-08-14 21:12:54 +0200

    Ajout de filtres sous forme de Material Chips

---
+++ src/app/chips/filters/filters.component.ts
@@ -0,0 +1,58 @@
+import { Component, OnInit, EventEmitter } from '@angular/core';
+import {COMMA, ENTER} from '@angular/cdk/keycodes';
+import {MatChipInputEvent} from '@angular/material/chips';
+
+
+export interface Filter {
+  name: string;
+}
+
+@Component({
+  selector: 'app-filters',
+  templateUrl: './filters.component.html',
+  styleUrls: ['./filters.component.scss']
+})
+export class FiltersComponent implements OnInit {
+  visible = true;
+  selectable = true;
+  removable = true;
+  addOnBlur = true;
+  readonly separatorKeysCodes: number[] = [ENTER, COMMA];
+  filters: Filter[] = [];
+  onChange: EventEmitter<boolean>;
+
+  constructor() {
+    this.onChange = new EventEmitter();
+  }
+
+  ngOnInit(): void {
+  }
+
+  add(event: MatChipInputEvent): void {
+    const input = event.input;
+    const value = event.value;
+
+    // Add our fruit
+    if ((value || '').trim()) {
+      this.filters.push({name: value.trim()});
+    }
+
+    // Reset the input value
+    if (input) {
+      input.value = '';
+    }
+    // Notify the new filter to the table
+    this.onChange.emit();
+  }
+
+  remove(filter: Filter): void {
+    const index = this.filters.indexOf(filter);
+
+    if (index >= 0) {
+      this.filters.splice(index, 1);
+    }
+    // Notify the  to the table
+    this.onChange.emit();
+  }
+
+}

efa08a88e6e6a6dddbd5f0d8b6fc531a1851f2a0
CH-KevinDubois <kvn.dubois@gmail.com>
2020-08-21 19:36:15 +0200

    Retrait des méthodes non utilisées

--- src/app/table/trip-table/trip-table-datasource.ts
+++ src/app/table/trip-table/trip-table-datasource.ts
@@ -72,54 +72,12 @@ export class TripsDataSource extends DataSource<Trip> {
       // stream for the data-table to consume.
       return this.tripsSubject.asObservable();
     }
-    //   const dataMutations = [
-    //     this.tripsSubject.asObservable(),
-    //     this.paginator.page,
-    //     this.sort.sortChange
-    //   ];
-      
-    //   return merge(...dataMutations).pipe(map(() => {
-    //     return this.getPagedData(this.getSortedData([...this.data]));
-    //   }));
-    // }
-    
+
     /**
     *  Called when the table is being destroyed. Use this function, to clean up
     * any open connections or free any held resources that were set up during connect.
     */
     disconnect() {}
     
-    /**
-    * Paginate the data (client-side). If you're using server-side pagination,
-    * this would be replaced by requesting the appropriate data from the server.
-    */
-    private getPagedData(data: Trip[]) {
-      const startIndex = this.paginator.pageIndex * this.paginator.pageSize;
-      return data.splice(startIndex, this.paginator.pageSize);
-    }
-    
-    /**
-    * Sort the data (client-side). If you're using server-side sorting,
-    * this would be replaced by requesting the appropriate data from the server.
-    */
-    private getSortedData(data: Trip[]) {
-      if (!this.sort.active || this.sort.direction === '') {
-        return data;
-      }
-      
-      return data.sort((a, b) => {
-        const isAsc = this.sort.direction === 'asc';
-        switch (this.sort.active) {
-          case 'title': return compare(a.title, b.title, isAsc);
-          case 'id': return compare(+a.id, +b.id, isAsc);
-          default: return 0;
-        }
-      });
-    }
-  }
-  
-  /** Simple sort comparator for example ID/Name columns (for client-side sorting). */
-  function compare(a: string | number, b: string | number, isAsc: boolean) {
-    return (a < b ? -1 : 1) * (isAsc ? 1 : -1);
   }
   

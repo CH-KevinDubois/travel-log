43d46101b50183dc1c02f5d9078db0ecec15aed6
CH-KevinDubois <kvn.dubois@gmail.com>
2020-09-22 22:04:44 +0200

    Utilisation de ViewChild pour la communication

--- src/app/api/dialogs/place-dialog/place-dialog.component.ts
+++ src/app/api/dialogs/place-dialog/place-dialog.component.ts
@@ -1,10 +1,12 @@
-import { Component, OnInit, Inject, OnDestroy, SimpleChange, SimpleChanges } from '@angular/core';
+import { Component, OnInit, Inject, OnDestroy, SimpleChange, SimpleChanges, ViewChild } from '@angular/core';
 import { FormControl, Validators, MaxLengthValidator } from '@angular/forms';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { Place } from 'src/app/models/place';
 import { MapManagementService } from '../../services/map-management.service';
 import { Observer, Observable, Subscription } from 'rxjs';
 import { GeoJsonLocation } from 'src/app/models/geo-json-location';
+import { latLng, LatLng } from 'leaflet';
+import { MapComponent } from 'src/app/map/map.component';
 
 @Component({
   selector: 'app-place-dialog',
@@ -21,9 +23,11 @@ export class PlaceDialogComponent implements OnInit {
 
   mapClickedSubscription: Subscription;
 
+  @ViewChild(MapComponent) map: MapComponent;
+
   constructor(
     public dialogRef: MatDialogRef<PlaceDialogComponent>,
-    private stateManagement: MapManagementService,
+    private mapManagement: MapManagementService,
     @Inject(MAT_DIALOG_DATA) public place: Place) 
     {
       this.nameControl = new FormControl('', [Validators.required, Validators.minLength(3), Validators.maxLength(100)]);
@@ -31,6 +35,15 @@ export class PlaceDialogComponent implements OnInit {
       this.pictureUrlControl = new FormControl('', [Validators.minLength(10), Validators.maxLength(500)]);
       this.longitudeControl = new FormControl('', [Validators.min(-180), Validators.max(180)]);
       this.latitudeControl = new FormControl('', [Validators.min(-90), Validators.max(90)]);  
+
+      if("geolocation" in navigator){
+        navigator.geolocation.getCurrentPosition( (position) => {
+          const latLng = new LatLng(position.coords.latitude, position.coords.longitude); 
+          this.map.drawCircle(latLng);
+          this.map.setZoom(14);
+          this.map.focusCoordinates(latLng);
+        });
+      }
     }
     
     onNoClick(): void {
@@ -39,7 +52,7 @@ export class PlaceDialogComponent implements OnInit {
     
     ngOnInit(): void {
 
-      this.mapClickedSubscription = this.stateManagement.clickedPointOnMap$.subscribe({
+      this.mapClickedSubscription = this.mapManagement.clickedPointOnMap$.subscribe({
         next: location => {
           this.place.location.coordinates[0] = location.coordinates[0];
           this.place.location.coordinates[1] = location.coordinates[1];

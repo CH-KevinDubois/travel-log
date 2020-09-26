5eb192064359dbf7bd99db9dd2757b9bff1e4c2c
CH-KevinDubois <kvn.dubois@gmail.com>
2020-09-26 18:11:19 +0200

    Api de test kevin

--- src/environments/environment.sample.ts
+++ src/environments/environment.sample.ts
@@ -1,5 +1,6 @@
 // Copy this file to environment.ts and fill in appropriate values.
 export const environment = {
     production: false,
-    apiUrl: "https://example.com/api",
+    apiUrl: "https://masrad-2020-tl-kevin.herokuapp.com/api",
+    url: "https://masrad-2020-tl-kevin.herokuapp.com"
   };
